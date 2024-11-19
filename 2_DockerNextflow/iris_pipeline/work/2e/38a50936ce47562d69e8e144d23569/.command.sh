6ff9a6e6aa422a2811bb1a9b807ef60cb1052ff9
unseltdm <137221721+unseltdm@users.noreply.github.com>
2024-11-19 12:55:22 -0500

    Delete 2_DockerNextflow/iris_pipeline/work directory

--- 2_DockerNextflow/iris_pipeline/work/2e/38a50936ce47562d69e8e144d23569/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ue
-# Call Python with the script and input file
-python3 /opt/pipeline/scripts/slice_data_final.py Iris.csv
--- 2_DockerNextflow/iris_pipeline/work/6e/1a451f6cbc1631ac8001780d2c16b6/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-# Call Python with the script and input file
-python3 /opt/pipeline/scripts/fit.py slice_1.csv
-mv linear_regression_output.csv "slice_1_linear_regression_output.csv"
